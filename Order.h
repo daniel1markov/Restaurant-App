93218fdd00a842a71a4a0e5d38ae44fddfe65b86
daniel <daniel1markov@gmail.com>
2021-04-01 23:52:48 +0300

    restaurant proj

---
+++ Order.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "non_alcoholic.h"
+#include "alcohol.h"
+#include "Fillet.h"
+#include "Entrecote.h"
+#include "Sinta.h"
+#include <vector>
+
+using namespace std;
+
+class Order
+{
+private:
+	vector<restaurant*> food;
+	int OrderNumber;
+	int amount;
+
+public:
+	Order();
+	double calculateTotalPrice();
+	void ReseTotalPrice();
+	int getAmount();
+	int getOrderNumber();
+	void addProduct(restaurant* );
+	void removeProduct(restaurant* );
+	restaurant* getProductBySerial(int );
+	vector<restaurant*> getAllProducts();
+};
+
+
+
+
